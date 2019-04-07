34e9529ab85536f0540fc8d1b87b117593e0409e
VishalTuran <49347841+VishalTuran@users.noreply.github.com>
2019-04-07 23:50:17 +0530

    Update sjf with preemption.c

--- sjf with preemption.c
+++ sjf with preemption.c
@@ -1,109 +1,90 @@
 #include<stdio.h>
-int n;
-struct process
+void Sorting();
+struct sjf
 {
+	int processno,arr_time,burst_time,wait_time,tar_time,complete_time;
+	int flag;
+	
+}process[100];
 
-int p_no;
-
-int arrival_t,burst_t,ct,wait_t,taround_time,p;
-
-int flag;
- }p_list[100];
- void Sorting()
- {
-struct process p;
-
-int i, j;
-
-for(i=0;i<n-1;i++)
-
+void main()
 {
-
-for(j=i+1;j<n;j++)
-
-{
-
-if(p_list[i].arrival_t > p_list[j].arrival_t)
-
-{
-
-p = p_list[i];
-
-p_list[i] = p_list[j];
-
-p_list[j] = p;
-
+	int i,t=0,b_t=0,peak;
+	int a[10],n;
+	float wait_time=0,taround_time=0,avg_w_t=0,avg_taround_time=0;
+	
+	printf("Enter the number of process:");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		process[i].processno=i+1;
+		printf("\nEnter details for Process %d process:",i+1);
+		printf("\nEnter Arrival time: ");
+		scanf("%d",&process[i].arr_time);
+		printf("Enter Burst time: ");
+		scanf("%d",&process[i].burst_time);
+		process[i].flag=0;
+		b_t=b_t+process[i].burst_time;		
+	}
+	Sorting();
+	for(i=0;i<n;i++)
+	{
+		a[i]=process[i].burst_time;
+		
+	}
+	process[9].burst_time=9999;
+	for(t=process[0].arr_time;t<b_t+1;t++)
+	{
+		peak=9;
+		for(i=0;i<n;i++)
+		{
+			if(process[i].arr_time<=t && process[i].burst_time<process[peak].burst_time && process[i].flag!=1)
+			{
+				peak=i;
+			}
+			if(process[peak].burst_time==0 &&process[i].flag!=1)
+			{
+				process[i].flag=1;
+				process[peak].complete_time=t;
+				process[peak].burst_time=9999;
+				printf("P%d complete in %d\n",process[i].processno,process[peak].complete_time);
+			}
+		}
+		(process[peak].burst_time)--;
+	}
+	for(i=0;i<n;i++)
+	{
+		process[i].tar_time=(process[i].complete_time)-(process[i].arr_time);
+		avg_taround_time=avg_taround_time+process[i].tar_time;
+		process[i].wait_time=((process[i].tar_time)-a[i]);
+		avg_w_t=avg_w_t+process[i].wait_time;
+	}
+	printf("PNO\tAT\tCT\tA\tWT\t\n");
+	for(i=0;i<n;i++)
+	{
+		printf("P%d\t%d\t%d\t%d\t%d\n",process[i].processno,process[i].arr_time,process[i].complete_time,process[i].tar_time,process[i].wait_time);
+		
+	}
+	printf("Average Turn around Time: %f\t\n\n",avg_taround_time);
+	printf("Average Waiting Time :\t %f\t\n",avg_w_t);	
+	
 }
 
-}
-
-}
- }
- int main()
- {
-
-int i,t=0,b_t=0,peak;
-
-int a[10];
-
-float wait_time = 0, taround_time = 0, avg_w_t=0, avg_taround_time=0;
-
-printf("enter the no. of processes: ");
-
-scanf("%d",&n);
-
-for(i = 0; i < n; i++)
-
+void Sorting()
 {
-
-p_list[i].p_no = i+1;
-
-printf("\nEnter Details For P%d process:-\n", p_list[i].p_no);
-printf("Enter Arrival Time: ");
-scanf("%d", &p_list[i].arrival_t );
-printf("Enter Burst Time: ");
-scanf("%d", &p_list[i].burst_t);
-p_list[i].flag = 0;
-b_t = b_t + p_list[i].burst_t;
-}
-Sorting();
-for(int i=0;i<n;i++)
-{
-a[i]=p_list[i].burst_t;
-}
-p_list[9].burst_t = 9999;
-for(t = p_list[0].arrival_t; t <= b_t+1;)
-{
-peak = 9;
-for(i=0;i<n;i++)
-{
-if(p_list[i].arrival_t <= t && p_list[i].burst_t < p_list[peak].burst_t && p_list[i].flag != 1)
-{
-peak = i;
-}
-if(p_list[peak].burst_t==0 && p_list[i].flag != 1)
-{
-p_list[i].flag = 1;
-p_list[peak].ct=t;p_list[peak].burst_t=9999;
-printf("P%d completes in %d\n",p_list[i].p_no,p_list[peak].ct);
-}
-}
-t++;
-(p_list[peak].burst_t)--;
-}
-for(i=0;i<n;i++)
-{
-p_list[i].taround_time=(p_list[i].ct)-(p_list[i].arrival_t);
-avg_taround_time=avg_taround_time+p_list[i].taround_time;
-p_list[i].wait_t=((p_list[i].taround_time)-a[i]);
-avg_w_t=avg_w_t+p_list[i].wait_t;
-}
-printf("PNO\tAT\tCT\tTA\tWTt\n");
-for(i=0;i<n;i++)
-{
-printf("P%d\t%d\t%d\t%d\t%d\n",p_list[i].p_no,p_list[i].arrival_t,p_list[i].ct,p_list[i].taround_time
-,p_list[i].wait_t);
- }
-printf("Average Turn around Time: %f\t\n\n",avg_taround_time);
-printf("Average Waiting Time :\t %f\t\n",avg_w_t);
+	struct sjf p;
+	int i,j,n;
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(process[i].arr_time>process[j].arr_time)
+			{
+				p=process[i];
+				process[i]=process[j];
+				process[j]=p;
+			}
+		}
+	}
 }
